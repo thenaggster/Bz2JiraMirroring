737e189a154c81ce7ce9ab0377f61d76018b6e44
thenaggster <rgarcia@redhat.com>
2020-11-05 16:52:21 -0500

    Updated filter & mapping of labels and fixVersion.

--- FixVersionMapping.java
+++ FixVersionMapping.java
@@ -1,13 +1,31 @@
 //Fix Version/s (id:fixVersions)
+//current
 {
     myFixVersion = "";
     if (bug.product == 'Container Native Virtualization (CNV)') {
-        myFixVersion = 'CNV v' + bug.target_release[0];
+        if (!bug.target_release[0].contains('---')){
+            myFixVersion = 'CNV v' + bug.target_release[0];
+        }
     };
     if (bug.product == 'OpenShift Container Platform') {
         bugTargetRelease =  bug.target_release[0].split('\\.');
-        myFixVersion = 'OpenShift ' + bugTargetRelease[0] + '.' + bugTargetRelease[1];
+        if (!bugTargetRelease.contains('---')){
+            myFixVersion = 'OpenShift ' + bugTargetRelease[0] + '.' + bugTargetRelease[1];
+        }
     }
 
+    myFixVersion;
+}
+
+//previous
+{   myFixVersion = "";     
+    if (bug.product == 'Container Native Virtualization (CNV)') {
+        myFixVersion = 'CNV v' + bug.target_release[0];
+    };
+    
+    if (bug.product == 'OpenShift Container Platform') {
+        bugTargetRelease =  bug.target_release[0].split('\\.');         
+        myFixVersion = 'OpenShift ' + bugTargetRelease[0] + '.' + bugTargetRelease[1];     
+    }      
     myFixVersion;
 }
--- LabelsMapping.java
+++ LabelsMapping.java
@@ -1,4 +1,6 @@
 //Labels (id:labels)
+
+//new
 {
     buglabels = new("java.util.ArrayList");
     
@@ -12,6 +14,31 @@
         buglabels.add(mykeyword);
     };
     
+    bugWhiteBoards =  bug.cf_internal_whiteboard.split(',');
+    for (myWhiteBoard : bugWhiteBoards) {
+        buglabels.add(myWhiteBoard);
+    };
+    
+    buglabels.add('cnvbugsm');
+    
+    buglabels;
+}
+
+
+//old
+{
+    buglabels = new("java.util.ArrayList");
+    
+    for (flag : bug.flags) {
+        if (flag.status == '+') {
+            buglabels.add(flag.name+flag.status);
+        }
+    };
+    
+    for (mykeyword : bug.keywords) {
+        buglabels.add(mykeyword);
+    };
+        
     buglabels.add(bug.cf_internal_whiteboard);
     
     buglabels.add('cnvbugsm');
