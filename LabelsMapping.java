8cc34012c8679f02bc083df6ca068757a4c87c2b
thenaggster <rgarcia@redhat.com>
2020-12-03 13:29:50 -0500

    Changes to better handle Internal Whiteboard label (split string and add individual elements of the array).

--- LabelsMapping.java
+++ LabelsMapping.java
@@ -14,32 +14,10 @@
         buglabels.add(mykeyword);
     };
     
-    bugWhiteBoards =  bug.cf_internal_whiteboard.split(',');
+    bugWhiteBoards =  bug.cf_internal_whiteboard.split(' ');
     for (myWhiteBoard : bugWhiteBoards) {
         buglabels.add(myWhiteBoard);
     };
-    
-    buglabels.add('cnvbugsm');
-    
-    buglabels;
-}
-
-
-//old
-{
-    buglabels = new("java.util.ArrayList");
-    
-    for (flag : bug.flags) {
-        if (flag.status == '+') {
-            buglabels.add(flag.name+flag.status);
-        }
-    };
-    
-    for (mykeyword : bug.keywords) {
-        buglabels.add(mykeyword);
-    };
-        
-    buglabels.add(bug.cf_internal_whiteboard);
     
     buglabels.add('cnvbugsm');
     
